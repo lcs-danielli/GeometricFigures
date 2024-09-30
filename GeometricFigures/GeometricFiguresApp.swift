6aae9178c92f231b15c1dfcc481240f30d393ca7
Daniel Li <danli@lcs.on.ca>
2024-09-30 14:56:44 -0400

    Initial Commit

---
+++ GeometricFigures/GeometricFiguresApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometricFiguresApp.swift
+//  GeometricFigures
+//
+//  Created by 李泽宇 on 2024-09-30.
+//
+
+import SwiftUI
+
+@main
+struct GeometricFiguresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
